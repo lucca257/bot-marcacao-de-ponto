061aa10770730dce2b280b336ba3b783841a62c5
Pedro Almeida <pedrolucca257@gmail.com.com>
2022-04-28 14:19:22 -0300

    feature: perguntas automatizadas

--- index.js
+++ index.js
@@ -1,4 +1,55 @@
 const marcar = require('./src/marcar-ponto');
 const login = require('./config');
+const readline = require('readline');
 
-marcar(login);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function question(theQuestion) {
+  return new Promise(resolve => rl.question(theQuestion, answ => resolve(answ)))
+}
+
+async function askQuestions(){
+  const data = new Date();
+  let dia = String(data.getDate()).padStart(2, '0');
+  let mes = String(data.getMonth() + 1).padStart(2, '0');
+  let ano = data.getFullYear();
+  var dados_ponto = {
+    horario_entrada : "09:00",
+    horario_entrada_intervalo : "12:00",
+    horario_saido_intervalo : "13:00",
+    horario_saida : "18:00",
+    data : dia + '/' + mes + '/' + ano,
+    ...login
+  }
+  let answer = await question("deseja marcar ponto padrao? (s/n):  ")
+  if(answer !== 's') {
+    dados_ponto.data = await question("qual data deseja marcar ? (dd/mm/aaaa)");
+    answer = await question("horario padrao? (s/n):  ")
+    if (answer !== 's') {
+      dados_ponto.horario_entrada = await question("horario I:  ");
+      dados_ponto.horario_entrada_intervalo = await question("horario II:  ");
+      dados_ponto.horario_saido_intervalo = await question("horario III:  ");
+      dados_ponto.horario_saida = await question("horario IV:  ");
+    }
+    console.log("dados do ponto: ", {
+      horario_entrada: dados_ponto.horario_entrada,
+      horario_entrada_intervalo: dados_ponto.horario_entrada_intervalo,
+      horario_saido_intervalo: dados_ponto.horario_saido_intervalo,
+      horario_saida: dados_ponto.horario_saida,
+    });
+    answer = await question("os horarios estao corretos ? (s/n):  ")
+    if (answer !== 's') {
+      rl.close();
+      return;
+    }
+    rl.close();
+    await marcar(dados_ponto);
+  }
+}
+
+(async () => {
+  await askQuestions();
+})();
